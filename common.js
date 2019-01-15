4ce892a51d96f79c66d277a7cbdb442a6e22d9f7
William Luo <gluo7777@gmail.com>
2019-01-14 19:20:06 -0600

    javascript practice

--- common.js
+++ common.js
@@ -1,6 +1,8 @@
 // meta-utility
 const log = console.log;
 
+/***************************** Helpful Code Snippets *****************************/
+
 // inline html
 const content = document.querySelector('#common');
 (function inlineHtml() {
@@ -13,3 +15,29 @@ const content = document.querySelector('#common');
     ul.innerHTML = html;
     content.appendChild(ul);
 })();
+
+// log both identifier and value
+const myObj = true;
+console.log({ myObj });
+
+/***************************** Concepts *****************************/
+
+/* Using 'new' operator vs Function only */
+let name = 'Bobby B';
+function Person(name) {
+    this.name = name;
+}
+
+// 'new' creates a copy of this Object and binds it to the 'this' inside the function.
+const person1 = new Person('William');
+// Function only - this inside function defaults to global this
+const person2 = Person('Jake');
+console.log(`Name of person1: ${person1.name}\n
+Name of person2: ${person2 === undefined ? 'person2 is undefined' : person2.name}\n
+Name of global name: ${this.name}`);
+
+/* Shared properties */
+// accomplished via function's prototype property
+const person3 = new Person('Henry');
+Person.prototype.country = 'USA';
+console.log(`person1 lives in same country as person3: ${person1.country === person3.country}, which would be ${person1.country}.`);

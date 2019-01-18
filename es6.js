f26f8bca220bdc0baf6d096bd6245a947ee8cbe1
William Luo <gluo7777@gmail.com>
2019-01-17 19:14:02 -0600

    canvas and es6 snippet

--- es6.js
+++ es6.js
@@ -3,3 +3,8 @@
 const nums = [1, 2, 3, 4, 5];
 nums.push(...[6, 7, 8, 9, 10]);
 log('Spreading: ', nums);
+
+// setting multiple variables in one line
+// destructuring an array
+const [first, last] = ['William', 'Luo'];
+log(first + ' ' + last);
